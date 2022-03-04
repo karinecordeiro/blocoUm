b2b3f08a17bfc0d5804b64d7f73090853826e499
KarineC <karine.cordeiro1998@hotmail.com>
2022-03-04 20:34:46 -0300

    quinto commit

--- Java/Introducao/Java/src/Repeticao/AtividadeQuatro.java
+++ Java/Introducao/Java/src/Repeticao/AtividadeQuatro.java
@@ -16,30 +16,47 @@ public class AtividadeQuatro {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);	
 		
-		int pessoa, idade, sexo, caracteristica, soma, sF, sM, sO, sC, sN, sA;
+		int pessoa, idade, c=0, mN=0, hA=0, oC=0, pN=0, pC=0;
+		char sexo, humor;
 		
 		System.out.print("Digite seu número de funcionário: ");
 		pessoa = ler.nextInt();
 			
-			while(pessoa>=1 && pessoa<=5)
+			while(pessoa>=1 && pessoa<=150)
 				{
 					System.out.print("\nPessoa n° "+pessoa++);
+					
 					System.out.print("\nDigite sua idade: ");
 					idade = ler.nextInt();
 					System.out.print("Digite o número referente ao sexo que você se identifica: (1)Feminino, (2)Masculino, (3)Outros: ");
-					sexo= ler.nextInt();
-					System.out.print("Digite o número de acordo como você se sente hoje: 1(Calme) 2(Nervose) 3(Agressive): ");
-					caracteristica= ler.nextInt();
+					sexo= ler.next().charAt(0);
+					System.out.print("Digite o número de acordo com seu humor: 1(Calme) 2(Nervose) 3(Agressive): ");
+					humor= ler.next().charAt(0);
+			
+					if(humor=='1') 
+						c++; 		/*c=Calme*/
 					
-				 	
-				 	
+					if(sexo=='1' && humor=='2') 
+						mN++; 					/*mN=Mulheres Nervosas*/
+					
+					if(sexo=='2' && humor=='3')
+						hA++;					 /*hA=Homens Agressivos*/
+					
+					if(sexo=='3' && humor=='1')
+						oC++; 					/*oC=Outros Calmes*/
+					
+					if(humor=='2' && idade>40)
+						pN++; 					/*pN=Pessoa Nervose*/
+					
+					if(humor=='1' && idade<18)
+						pC++; 					/*pC=Pessoa Calme*/
 				}
-			System.out.print("\nO número de pessoas calmas é igual a: ");
-			System.out.print("\nO número de mulheres nervosas é igual a: ");
-			System.out.print("\nO número de homens agressivos é igual a: ");
-			System.out.print("\nO número de outros calmos é igual a: ");
-			System.out.print("\nO número de pessoas nervosas com mais de 40 anos é igual a: ");
-			System.out.print("\nO número de pessoas calmas com menos de 18 é igual a: ");
+			
+			System.out.print("\nO número de pessoas calmas é igual a: "+c);
+			System.out.print("\nO número de mulheres nervosas é igual a: "+mN);
+			System.out.print("\nO número de homens agressivos é igual a: "+hA);
+			System.out.print("\nO número de outros calmos é igual a: "+oC);
+			System.out.print("\nO número de pessoas nervosas com mais de 40 anos é igual a: "+pN);
+			System.out.print("\nO número de pessoas calmas com menos de 18 é igual a: "+pC);
 	}
-
 }
