408053f20347d74813058ed71ab06408418457dc
KarineC <karine.cordeiro1998@hotmail.com>
2022-03-10 12:29:52 -0300

    Decido commit

---
+++ Java/Introducao/Java/src/Colecao/EstoqueLoja.java
@@ -0,0 +1,51 @@
+package Colecao;
+
+/*Crie uma um programa para trabalhar com estoque de uma loja, o programa deverá 
+trabalhar com Collection do tipo List do Java para manipular os dados desse estoque,
+o programa deverá atender as seguintes funcionalidades:
+--Armazenar dados da List
+--Remover dados da list;
+--Atualizar dados da list.
+--Apresentar todos os dados da list.*/
+
+public class EstoqueLoja {
+
+		private String roupa;
+		private int tamanho;
+		private int quantidade;
+		
+		public EstoqueLoja(String r, int t, int q)
+		{
+			this.roupa=r;
+			this.tamanho=t;
+			this.quantidade=q;
+		}
+
+		public String getRoupa() {
+			return roupa;
+		}
+
+		public void setRoupa(String roupa) {
+			this.roupa = roupa;
+		}
+
+		public int getTamanho() {
+			return tamanho;
+		}
+
+		public void setTamanho(int tamanho) {
+			this.tamanho = tamanho;
+		}
+
+		public int getQuantidade() {
+			return quantidade;
+		}
+
+		public void setQuantidade(int quantidade) {
+			this.quantidade = quantidade;
+		}
+		public String toString()
+		{
+			return "Estoque: "+this.roupa+". Tamanho: "+this.tamanho+". Quantidade: "+this.quantidade+ "\t";
+		}
+}
---
+++ Java/Introducao/Java/src/Colecao/ObjetoEstoque.java
@@ -0,0 +1,45 @@
+package Colecao;
+
+/*--Armazenar dados da List
+--Remover dados da list;
+--Atualizar dados da list.
+--Apresentar todos os dados da list.*/
+
+import java.util.ArrayList;
+
+public class ObjetoEstoque {
+
+	public static void main(String[] args) {
+		 
+			EstoqueLoja e1= new EstoqueLoja("Blusa", 16, 6);
+			EstoqueLoja e2= new EstoqueLoja("Calça", 42, 11);
+			EstoqueLoja e3= new EstoqueLoja("Sapato", 38, 5);
+			EstoqueLoja e4= new EstoqueLoja("Vestido", 40, 2);
+			EstoqueLoja e5= new EstoqueLoja("Vestido", 40, 0);
+			
+			ArrayList<EstoqueLoja> estoque = new ArrayList<>();
+			
+			estoque.add(e1);
+			estoque.add(e2);
+			estoque.add(e3);
+			estoque.add(e4);
+			
+			System.out.print("ESTOQUE APRESENTADO\n");
+			
+			System.out.print("Adicionado "+estoque+"\n");
+			
+			
+			estoque.set(3, e5);
+			System.out.println("Atualizado "+estoque);
+			
+			estoque.remove(e5);
+			System.out.println("Removido "+estoque);
+			
+			
+			
+		
+			
+			
+	}	
+
+}
